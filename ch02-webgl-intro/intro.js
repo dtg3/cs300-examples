d33b2c8bc7a72c8145bd4c6deb4ff0e61dad9c38
Drew Guarnera <dguarnera@wooster.edu>
2021-09-01 13:35:02 -0400

    Working point display

--- ch02-webgl-intro/intro.js
+++ ch02-webgl-intro/intro.js
@@ -16,5 +16,22 @@ window.onload = function init() {
 
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0, 0, 0 , 1.0);
-    
+
+    var programs = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(programs);
+
+    var point_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, point_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+    var vertex_position = gl.getAttribLocation(programs, "vPosition");
+    gl.vertexAttribPointer(vertex_position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vertex_position);
+
+    render();
+}
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, points.length);
 }
