47c7664fc85738d22bb3c246e9cad1cc7ce905b5
Drew Guarnera <dguarnera@wooster.edu>
2021-11-03 14:12:49 -0400

    Add features to Rollball example
    Add sound effects, "death" trigger collisions, improved jumping,
    and a Physic surface.

--- unity-examples/Rollball/Assets/scripts/PlayerController.cs
+++ unity-examples/Rollball/Assets/scripts/PlayerController.cs
@@ -6,21 +6,28 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
+    [SerializeField] private float speed = 0;
     private int score;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
-    public TextMeshProUGUI countText;
-    public GameObject winTextObject;
-    private bool jump;
-    [SerializeField] int jumpForce = 10;
+    [SerializeField] private TextMeshProUGUI countText;
+    [SerializeField] private GameObject winTextObject;
+    [SerializeField] private int jumpForce = 10;
+    [SerializeField] private int gravityScale;
+    private int remainingJumps;
+    private const int MAX_JUMPS = 1;
+
+    [SerializeField] private AudioSource jumpSound;
+    [SerializeField] private AudioSource hitGroundSound;
+    [SerializeField] private AudioSource pickupSound;
     // Start is called before the first frame update
 
     void Awake() 
     {
-        jump = false;
+        remainingJumps = MAX_JUMPS;
     }
+
     void Start()
     {   
         score = 0;
@@ -35,12 +42,23 @@ public class PlayerController : MonoBehaviour
         if (score == 14) {
             winTextObject.SetActive(true);
         }
+
+        if (!gameObject.activeInHierarchy)
+        {
+            TextMeshProUGUI win = winTextObject.GetComponent<TextMeshProUGUI>();
+            win.text = "YOU LOSE\nGAME OVER!";
+            winTextObject.SetActive(true);
+        }
     }
 
     void OnJump()
     {
-        Debug.Log("JUMP!");
-        jump = true;
+        if (remainingJumps > 0) 
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            --remainingJumps;
+            jumpSound.Play();
+        }
     }
 
     void OnMove(InputValue movementValue)
@@ -52,13 +70,9 @@ public class PlayerController : MonoBehaviour
     
     void FixedUpdate()
     {
+        rb.AddForce(Physics.gravity * gravityScale * rb.mass);
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
-        
-        if (jump) {
-            rb.AddForce(Vector3.up * jumpForce);
-            jump = false;
-        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -67,7 +81,20 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             score = score + 1;
+            pickupSound.Play();
             SetCountText();
         }
+        else if (other.gameObject.CompareTag("DeadZone"))
+        {
+            gameObject.SetActive(false);
+            SetCountText();
+        }
+    }
+
+    private void OnCollisionEnter(Collision objectCollision)
+    {
+        Debug.Log("HIT SURFACE");
+        remainingJumps = MAX_JUMPS;
+        hitGroundSound.Play();
     }
 }
