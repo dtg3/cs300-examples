ae420ea9a9f1f16446e4e47bdab2983a842991db
Drew Guarnera <dguarnera@wooster.edu>
2021-12-01 19:07:20 -0500

    Update animation example to setup a blend tree

--- unity-examples/AnimationBasics/Assets/Scripts/AnimationStateController.cs
+++ unity-examples/AnimationBasics/Assets/Scripts/AnimationStateController.cs
@@ -3,22 +3,52 @@ using UnityEngine;
 public class AnimationStateController : MonoBehaviour
 {
     private Animator animator;
+    private float velocity = 0.0f;
+    [SerializeField] private float acceleration = 0.1f;
+
+    // For the oldBooleanAnimation
+    private int isWalkingHash;
+    private int isRunningHash;
+    int velocityHash;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+
+        // For the oldBooleanAnimation
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+    }
+
+    void oldBooleanAnimation(bool forwardPressed, bool runPressed)
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (!isRunning && (forwardPressed &&  runPressed))
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        if (isRunning && (!forwardPressed || !runPressed)) {
+            animator.SetBool(isRunningHash, false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w")) {
-            animator.SetBool("isWalking", true);
-        }
-
-        if (!Input.GetKey("w")) {
-            animator.SetBool("isWalking", false);
-        }
-        
+        bool forwardPressed = Input.GetKey("w");
+        bool shiftPressed = Input.GetKey("left shift");
     }
 }
