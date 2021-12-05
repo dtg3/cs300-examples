9729f6525c537642fd94ed07c6c61265c10aec12
Drew Guarnera <dguarnera@wooster.edu>
2021-12-05 10:32:41 -0500

    Add 1D blend tree to animation

--- unity-examples/AnimationBasics/Assets/Scripts/AnimationStateController.cs
+++ unity-examples/AnimationBasics/Assets/Scripts/AnimationStateController.cs
@@ -5,6 +5,7 @@ public class AnimationStateController : MonoBehaviour
     private Animator animator;
     private float velocity = 0.0f;
     [SerializeField] private float acceleration = 0.1f;
+    [SerializeField] private float deceleration = 0.5f;
 
     // For the oldBooleanAnimation
     private int isWalkingHash;
@@ -18,6 +19,7 @@ public class AnimationStateController : MonoBehaviour
         // For the oldBooleanAnimation
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
+        velocityHash = Animator.StringToHash("Velocity");
     }
 
     void oldBooleanAnimation(bool forwardPressed, bool runPressed)
@@ -50,5 +52,32 @@ public class AnimationStateController : MonoBehaviour
     {
         bool forwardPressed = Input.GetKey("w");
         bool shiftPressed = Input.GetKey("left shift");
+
+        if (forwardPressed && shiftPressed && velocity < 1.0f)
+        {
+            velocity += Time.deltaTime * acceleration;
+        }
+
+        if (forwardPressed && !shiftPressed)
+        {
+            if (velocity < 0.5f)
+                velocity += Time.deltaTime * acceleration;
+            
+            if (velocity > 0.5f)
+                velocity -= Time.deltaTime * deceleration;
+            
+        }
+
+        if (!forwardPressed && velocity > 0.0f)
+        {
+            velocity -= Time.deltaTime * deceleration;
+        }
+
+        if (!forwardPressed && velocity < 0.0f)
+        {
+            velocity = 0.0f;
+        }
+
+        animator.SetFloat(velocityHash, velocity);
     }
 }
