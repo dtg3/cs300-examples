bf5dad3718b58526d22ef75468ba6160e8491be3
Drew Guarnera <dguarnera@wooster.edu>
2021-12-10 15:07:28 -0500

    Update navmesh with click to move

---
+++ unity-examples/SimpleNavMesh/Assets/NavMeshAgentController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentController : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
